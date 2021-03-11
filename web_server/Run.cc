f9f4ce2c98d685069c5f0ec75c263b9ad37458af
bigpyj <bigpyj@foxmail.com>
2021-03-11 22:21:01 +0800

    Fix args in main

--- web_server/Run.cc
+++ web_server/Run.cc
@@ -7,15 +7,19 @@
 
 int main(int argc, char* argv[]) {
     int num_threads = 0;
+    bool use_timer = false;
     if (argc > 1) {
         num_threads = atoi(argv[1]);
+        if (argc > 2) {
+            use_timer = true;
+        }
     }
     
     std::string image_path;
     image_path += "./image.jpg";
 
     muduo::net::EventLoop loop;
-    Server server(&loop, InetAddress(8000), "Naive Server", false, image_path);
+    Server server(&loop, InetAddress(8000), "Naive Server", use_timer, image_path);
     server.setThreadNum(num_threads);
     server.start();
     loop.loop();
